94246e6c6f003e2b525e0aa721511227633e7e9c
kping0 <36621476+kping0@users.noreply.github.com>
2018-04-15 04:25:34 -0400

    Add files via upload

--- serialization.c
+++ serialization.c
@@ -3,45 +3,75 @@
 #include <stdio.h> 
 #include <assert.h>
 #include <string.h> 
+
 #include "base64.h" 
 #include "serialization.h" 
 /*
  * SimpleSecureChat Serialization Library. Made with Security in mind.
 */
-
-sscso *SSCS_object(){
-	sscso* obj = malloc(sizeof(struct SSCS_struct));		
-	obj->buf_ptr = NULL; obj->allocated = 0;
+byte* memseq(byte* buf,size_t buf_size,byte* byteseq,size_t byteseq_len){ size_t i = 0;size_t x = 0;byte* firstbyte = NULL; //Initialize variables
+	while(1){
+		if(!(i < buf_size))return NULL;
+		if(buf[i] == byteseq[x]){
+			firstbyte = (buf+i); //Pointer to be returned if we find the seq
+			x++;i++;
+			while(1){	
+				if(x == byteseq_len)return firstbyte; //we found the byte sequence
+				else if(!(i < buf_size))return NULL; //exit,have reached buf_size
+				else if(buf[i] == byteseq[x]){x++;i++;} //byte's match,continue
+				else{x = 0;firstbyte = NULL;break;} //byte's didnt match,reset
+				
+			}		
+		}	
+		else{i++;} //If byte's dont match add increase the index 
+	}	
+}
+sscso *SSCS_object(void){
+	sscso* obj = calloc(1,sizeof(struct SSCS_struct));		
+	obj->buf_ptr = NULL; 
+	obj->allocated = 0;
 	return obj;
+}
+sscsl *SSCS_list(void){
+	sscsl* list = calloc(1,sizeof(struct SSCS_list));
+	list->buf_ptr = NULL;
+	list->allocated = 0;
+	list->items = 0;
+	list->readloc = 0;
+	return list;
+}
+sscsl *SSCS_list_open(byte* buf){
+	size_t len = strlen((const char*)buf);
+	void* buf_ptr = calloc(1,len);
+	memcpy(buf_ptr,buf,len);
+	sscsl *list = calloc(1,sizeof(struct SSCS_list));
+	list->buf_ptr = buf_ptr;
+	list->allocated = len;
+	return list;
 }
 
 sscso *SSCS_open(byte* buf){
 	size_t len = strlen((const char*)buf);
-	byte* buf_ptr = malloc(len);
+	void* buf_ptr = calloc(1,len);
 	memcpy(buf_ptr,buf,len);
-	sscso* obj = malloc(sizeof(struct SSCS_struct));
+	sscso* obj = calloc(1,sizeof(struct SSCS_struct));
 	obj->buf_ptr = buf_ptr;
 	obj->allocated = len;
 	return obj;
 }
-int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size){
+int SSCS_list_add_data(sscsl* list,byte* data,size_t size){
 	if(size <= 0)return -1; //Size has to be bigger than 0
-	void *old_buf_ptr = obj->buf_ptr;
-	size_t old_buf_size = obj->allocated;	
+	void *old_buf_ptr = list->buf_ptr;
+	size_t old_buf_size = list->allocated;	
+	size_t old_buf_items = list->items;
 	size_t encoded_size;
-	if(old_buf_ptr != NULL){
-		byte* validationpointer = (byte*)strstr((const char*)old_buf_ptr,label);
-		if(validationpointer != NULL){
-			puts("Label Already Exists");	
-			return -1;
-		}
-	}
-
 	byte* b64data = base64_encode(data,size,&encoded_size);
 	int b64datalen = encoded_size;
+	char label[12];
+	sprintf(label,"%zd:\"",old_buf_items+1);
 	int label_len = strlen((const char*)label);
-	size_t final_intermediate_len = b64datalen+label_len+4;
-	byte* intermediatebuf = malloc(final_intermediate_len);
+	size_t final_intermediate_len = b64datalen+label_len+2;
+	byte* intermediatebuf = calloc(1,final_intermediate_len);
 	memset(intermediatebuf,0,final_intermediate_len);
 	byte* ibufwloc = intermediatebuf; //ibufwloc is a pointer to the next area to write to
 	int i = 0;
@@ -50,10 +80,6 @@ int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size){
 		ibufwloc++;
 		i++;
 	}
-	*ibufwloc = ':';
-	ibufwloc++;
-	*ibufwloc = '"';
-	ibufwloc++;
 	memcpy(ibufwloc,b64data,encoded_size); //Add B64 encoded data 
 	ibufwloc += b64datalen;
 	*(ibufwloc) = '"';
@@ -62,7 +88,7 @@ int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size){
 	ibufwloc++;
 	free(b64data);	
 	size_t new_buf_size = (old_buf_size + final_intermediate_len);
-	void *new_buf_ptr = malloc(new_buf_size+1);
+	void *new_buf_ptr = calloc(1,new_buf_size);
 	memset(new_buf_ptr,0,new_buf_size);
 	if(old_buf_ptr != NULL)memcpy(new_buf_ptr,old_buf_ptr,old_buf_size);
 	free(old_buf_ptr);
@@ -74,43 +100,99 @@ int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size){
 		base_ptr++;
 		i++;
 	}
-	*(byte*)(new_buf_ptr+new_buf_size) = '\0';
+	list->allocated = new_buf_size;
+	list->buf_ptr = new_buf_ptr;
+	list->items = old_buf_items+1;
+	free(intermediatebuf);
+	return 0;
+}
+int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size){
+	if(size <= 0)return -1; //Size has to be bigger than 0
+	void *old_buf_ptr = obj->buf_ptr;
+	size_t old_buf_size = obj->allocated;	
+	int label_len = (int)strlen((const char*)label);
+	int modlabel_len = label_len+2;
+	char* modlabel = calloc(1,modlabel_len+1); //+1 for the NUL 
+	sprintf(modlabel,"%s:\"",label);
+	size_t encoded_size;
+	if(old_buf_ptr != NULL){
+		byte* validationpointer = memseq((byte*)old_buf_ptr,old_buf_size,(byte*)modlabel,modlabel_len);
+		if(validationpointer != NULL){
+			//puts("Label Already Exists");	
+			return -1;
+		}
+	}
+
+	byte* b64data = base64_encode(data,size,&encoded_size);
+	int b64datalen = encoded_size;
+	size_t final_intermediate_len = b64datalen+modlabel_len+2; //+2 for the ' "; ' at the end
+	byte* intermediatebuf = calloc(1,final_intermediate_len);
+	byte* ibufwloc = intermediatebuf; //ibufwloc is a pointer to the next area to write to
+	size_t i = 0;
+	while(i < strlen(modlabel)){
+		*ibufwloc = modlabel[i];
+		ibufwloc++;
+		i++;
+	}
+	memcpy(ibufwloc,b64data,encoded_size); //Add B64 encoded data 
+	ibufwloc += b64datalen;
+	*(ibufwloc) = '"';
+	ibufwloc++;
+	*ibufwloc = ';';
+	ibufwloc++;
+	free(b64data);	
+	size_t new_buf_size = (old_buf_size + final_intermediate_len);
+	void *new_buf_ptr = calloc(1,new_buf_size);
+	memset(new_buf_ptr,0,new_buf_size);
+	if(old_buf_ptr != NULL)memcpy(new_buf_ptr,old_buf_ptr,old_buf_size);
+	free(old_buf_ptr);
+	old_buf_ptr = NULL;
+	i = 0;
+	byte* base_ptr = new_buf_ptr + old_buf_size ;	
+	while(i < final_intermediate_len){
+		*base_ptr = intermediatebuf[i];	
+		base_ptr++;
+		i++;
+	}
 	obj->allocated = new_buf_size;
 	obj->buf_ptr = new_buf_ptr;
 	free(intermediatebuf);
+	free(modlabel);
 	return 0;
 }
 sscsd* SSCS_object_data(sscso* obj,char* label){
 	byte* buf_ptr = obj->buf_ptr;
 	size_t allocated = obj->allocated;
 	size_t label_len = strlen((const char*)label);
-	byte* readpointer = (byte*)strstr((const char*)buf_ptr,label);
+	byte* readpointer = memseq(buf_ptr,allocated,(byte*)label,label_len);
 	if(readpointer == NULL){
-		puts("Label Not Found");
+		//puts("Label Not Found");
+		//printf("Label Not Found %s\n",label);
 		return NULL;
 	}
 	readpointer+=label_len;
 	if(readpointer[0] != ':' || readpointer[1] != '"'){
-		puts("Label Error");
+		//puts("Label Error");
 		return NULL;
 	}
+
 	readpointer+=2;
 	//Readpointer is now at the beginning of the base64 encoded data
 	int i = 0;
 	while(readpointer[i] != '"' && readpointer[i+1] != ';'){ //Run once to get length of string 
 		if(!((readpointer+i)-buf_ptr < (signed)allocated)){
-			puts("Outsize of memory bounds");
+			//puts("Outsize of memory bounds");
 			return NULL;
 		}
 		i++;	
 	}
 	double b64encoded_len = i; 
-	byte* b64buffer = malloc(b64encoded_len+1); 
+	byte* b64buffer = calloc(1,b64encoded_len+1); 
 	i = 0;
 	/* Run loop again to read encoded string */
 	while(readpointer[i] != '"' && readpointer[i+1] != ';'){ //Get base64encoded data 
 		if(!((size_t)((readpointer+i) - buf_ptr) < allocated) || b64encoded_len < i){
-			puts("Outsize of memory bounds");
+			//puts("Outsize of memory bounds");
 			free(b64buffer);
 			return NULL;
 		}
@@ -118,20 +200,75 @@ sscsd* SSCS_object_data(sscso* obj,char* label){
 		i++;
 	}
 	size_t len; 
-	sscsd* final = malloc(sizeof(sscsd));
+	sscsd* final = calloc(1,sizeof(sscsd));
 	final->data = base64_decode((const unsigned char*)b64buffer,b64encoded_len,&len); //NOTE THAT THIS IS A POINTER (if an integer was serialized an (int*) )
 	final->len = len;
 	free(b64buffer);
 	return final;
 }
-
+sscsd* SSCS_list_data(sscsl* list,unsigned int index){ //Auto incriments after every run.
+	if(index > 4096)return NULL; //support a max of 4096 items
+	byte* buf_ptr = list->buf_ptr;
+	size_t allocated = list->allocated;
+	char* label = calloc(1,30);
+	sprintf(label,"%d:\"",index);
+	size_t label_len = strlen((const char*)label);
+	int i = 0;
+	byte* readpointer = memseq(buf_ptr,allocated,(byte*)label,label_len);
+	if(readpointer == NULL){
+		//printf("Label Not Found %s\n",label);
+		return NULL;
+	}
+	readpointer+=label_len;
+	//Readpointer is now at the beginning of the base64 encoded data
+	i = 0;
+	while(readpointer[i] != '"' && readpointer[i+1] != ';'){ //Run once to get length of string 
+		if(!((readpointer+i)-buf_ptr < (signed)allocated)){
+			//puts("Outsize of memory bounds");
+			return NULL;
+		}
+		i++;	
+	}
+	double b64encoded_len = i; 
+	byte* b64buffer = calloc(1,b64encoded_len+1); 
+	i = 0;
+	/* Run loop again to read encoded string */
+	while(readpointer[i] != '"' && readpointer[i+1] != ';'){ //Get base64encoded data 
+		if(!((size_t)((readpointer+i) - buf_ptr) < allocated) || b64encoded_len < i){
+			//puts("Outsize of memory bounds");
+			free(b64buffer);
+			return NULL;
+		}
+		*(b64buffer+i) = *(readpointer+i);	
+		i++;
+	}
+	size_t len; 
+	sscsd* final = calloc(1,sizeof(sscsd));
+	final->data = base64_decode((const unsigned char*)b64buffer,b64encoded_len,&len); //NOTE THAT THIS IS A POINTER (if an integer was serialized an (int*) )
+	final->len = len;
+	free(b64buffer);
+	free(label);
+	return final;
+}
 char* SSCS_object_encoded(sscso* obj){ //Get string to send over socket
-	return obj->buf_ptr;
+	char* retptr = calloc(1,obj->allocated+2);
+	memcpy(retptr,obj->buf_ptr,obj->allocated);
+	*(retptr+obj->allocated+1) = '\0';	
+	return retptr;
 }
 
 size_t SSCS_object_encoded_size(sscso* obj){ //Get size of string(often needs to be specified when sending over socket)
 	return obj->allocated;	
 } 
+char* SSCS_list_encoded(sscsl* list){
+	char* retptr = calloc(1,list->allocated+1);
+	memcpy(retptr,list->buf_ptr,list->allocated);
+	*(retptr+list->allocated) = '\0';
+	return retptr;
+}
+size_t SSCS_list_encoded_size(sscsl* list){
+	return list->allocated;
+}
 
 byte* SSCS_data_get_data(sscsd* data){
 	return data->data;
@@ -140,11 +277,13 @@ byte* SSCS_data_get_data(sscsd* data){
 size_t SSCS_data_get_size(sscsd* data){
 	return data->len;
 }
+/* UNUSED because all functions allocated a new sscso object and dont reuse 
 void SSCS_free(sscso* obj){ //Frees current buffer associated with sscso obj (Object Can be Reused)
 	free(obj->buf_ptr);
 	obj->buf_ptr = NULL;
 	obj->allocated = 0;
 }
+*/
 
 void SSCS_release(sscso** obj){ //Frees the current buffer AND the structure holding the address to the buffer; (Object Cant be Reused)
 	free(((*obj)->buf_ptr));
@@ -157,6 +296,12 @@ void SSCS_data_release(sscsd** data){ //Frees the data buffer AND the structure 
 	free(*data);
 	*data = NULL;	
 }
+void SSCS_list_release(sscsl** list){
+	if(*list == NULL)return;
+	free(((*list)->buf_ptr));
+	free(*list);
+	*list = NULL;
+}
 /*
 * Wrappers for SSCS_object_data() to simplify usage 
 */
@@ -164,7 +309,7 @@ int SSCS_object_int(sscso* obj,char* label){
 	sscsd* data = SSCS_object_data(obj,label);
 	if(data == NULL)return -1;
 	if(data->len != sizeof(int)){
-		puts("Data stored with label is not an integer");
+		//puts("Data stored with label is not an integer");
 		SSCS_data_release(&data);
 		return -1;
 	}
@@ -176,7 +321,7 @@ double SSCS_object_double(sscso* obj,char* label){
 	sscsd* data = SSCS_object_data(obj,label);
 	if(data == NULL)return -1;
 	if(data->len != sizeof(double)){
-		puts("Data stored with label is not a double");
+		//puts("Data stored with label is not a double");
 		SSCS_data_release(&data);
 		return -1;
 	}	
@@ -187,7 +332,7 @@ double SSCS_object_double(sscso* obj,char* label){
 unsigned char* SSCS_object_string(sscso* obj,char* label){
 	sscsd* data = SSCS_object_data(obj,label);
 	if(data == NULL)return NULL;
-	unsigned char* ret_ptr = malloc(data->len+2);
+	unsigned char* ret_ptr = calloc(1,data->len+2);
 	memcpy(ret_ptr,data->data,data->len);
 	*(ret_ptr+data->len + 1) = '\0';
 	SSCS_data_release(&data);
--- serialization.h
+++ serialization.h
@@ -10,39 +10,65 @@ struct SSCS_data{ //Object returned by SSCS_object_data
 	void* data; //Data returned by function
 	size_t len; //Length of data
 };
+struct SSCS_list{
+	void* buf_ptr; //Pointer to current buffer
+	size_t allocated; //Size of current buffer
+	size_t items; //items in buf_ptr;
+	size_t readloc; //used when reading from the list
+};
 
 typedef struct SSCS_struct sscso; //SimpleSecureChatSerialized_Object
+
 typedef struct SSCS_data sscsd; //SimpleSecureChatSerialized_Data
+
+typedef struct SSCS_list sscsl; //SimpleSecureChatSerialized_List
 
 typedef unsigned char byte;
 
-sscso *SSCS_object(void);
 
-sscso *SSCS_open(byte* buf);
+byte* memseq(byte* buf,size_t buf_size,byte* byteseq,size_t byteseq_len); //Search memory buf with sizeof buf_size for byte sequence byteseq with sizeof byteseq_len and return ptr to first occurence
+	
+sscso *SSCS_object(void); //Generate SSCS_object
 
-int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size);
+sscsl *SSCS_list(void); //Generate SSCS_list
 
-sscsd* SSCS_object_data(sscso* obj,char* label);
+sscso *SSCS_open(byte* buf); //Open an encoded sscs object
 
-char* SSCS_object_encoded(sscso* obj);
+sscsl *SSCS_list_open(byte* buf); //open an encoded list
 
-size_t SSCS_object_encoded_size(sscso* obj);
+int SSCS_object_add_data(sscso* obj,char* label,byte* data,size_t size); //adds data to a SSCS_object 
 
-byte* SSCS_data_get_data(sscsd* data);
+sscsd* SSCS_object_data(sscso* obj,char* label); //returns SSCS_data struct that has to be freed using SSCS_data_release(&data_structure)
 
-size_t SSCS_data_get_size(sscsd* data);
+int SSCS_list_add_data(sscsl* list,byte* data,size_t size); //add data to list 
 
-void SSCS_free(sscso* obj);
+sscsd* SSCS_list_data(sscsl* list,unsigned int index); //get data from list with an index of i (i < 4096)
 
-void SSCS_release(sscso** obj);
+char* SSCS_object_encoded(sscso* obj); //get encoded version of SSCS_object
 
-void SSCS_data_release(sscsd** data);
+size_t SSCS_object_encoded_size(sscso* obj); //get size of sscs_encoded object
 
-int SSCS_object_int(sscso* obj,char* label);
+char* SSCS_list_encoded(sscsl* list); //get encoded version of SSCS_list
 
-double SSCS_object_double(sscso* obj,char* label);
+size_t SSCS_list_encoded_size(sscsl* list); //get size of sscs encoded list
 
-unsigned char* SSCS_object_string(sscso* obj,char* label);
+byte* SSCS_data_get_data(sscsd* data); //equivalent of doing : ... setup sscsd* data_obj...; data_obj->data
+
+size_t SSCS_data_get_size(sscsd* data); //data_obj->len
+
+//void SSCS_free(sscso* obj); //Useless
+
+void SSCS_release(sscso** obj); //release associated pointers and struct itself (has to be passed a reference eg. SSCS_release(&obj) 
+
+void SSCS_data_release(sscsd** data); //release associated pointers and struct itself SSCS_data_release(&data)
+
+void SSCS_list_release(sscsl** list); //s.o SSCS_list_release(&list)
+
+int SSCS_object_int(sscso* obj,char* label); //wrapper around SSCS_object_data
+
+double SSCS_object_double(sscso* obj,char* label); //wrapper around SSCS_object_data
+
+unsigned char* SSCS_object_string(sscso* obj,char* label); //wrapper around SSCS_object_data
 
 #endif /* SSCS_SERIALIZATION */
 
